d04bb84286fad0845f1507bdf693414d1dce6a7f
shrikantdandge665 <shrikantdandge665@gmail.com>
2023-09-14 22:39:09 +0530

    made changes to backend.tf

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "10weeksofcloudops-week4"
+    bucket = "eks-demo-10weeks"
     key    = "backend/ToDo-App.tfstate"
     region = "us-east-1"
     dynamodb_table = "10weeksofcloudops-dynamo"
+    shared_credentials_file = "$HOME/.aws/credentials"
+    profile = "default"
   }
 }
